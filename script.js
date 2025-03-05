aa4a008bcc06cd1dee86c980b3248671fcd95058
jenniferbogado1 <127226927+jenniferbogado1@users.noreply.github.com>
2025-03-05 17:56:04 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -4,10 +4,38 @@ document.addEventListener("DOMContentLoaded", () => {
     loadWatchList();
 });
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDFezlQ68RTmss1HFnZyWFAmhjm8bstqAU",
+  authDomain: "trackermovie-10156.firebaseapp.com",
+  projectId: "trackermovie-10156",
+  storageBucket: "trackermovie-10156.firebasestorage.app",
+  messagingSenderId: "1062742578352",
+  appId: "1:1062742578352:web:b512d48c4aba27bb449775",
+  measurementId: "G-GT8CZQKM5C"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
+
+
 // Navegación entre secciones
 function navigateTo(section) {
     window.location.hash = section;
 }
+
 
 
 function generateStars(score) {
